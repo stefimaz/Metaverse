18b453a6bab09633603a38fc5a5a229d09c3b7e3
stefimaz <89653430+stefimaz@users.noreply.github.com>
2022-05-06 15:08:41 -0400

    Solidity contract started

--- metaverse/src/contracts/Land.sol
+++ metaverse/src/contracts/Land.sol
@@ -1,1 +1,44 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract Land is ERC721 {
+    uint256 public cost = 1 ether;
+    uint256 public maxsuppy = 5;
+    uint256 public totalSupply = 0;
+
+    struct Building {
+        string name;
+        address owner;
+        int256 posX;
+        int256 posY;
+        int256 posZ;
+        uint256 sizeX;
+        uint256 sizeY;
+        uint256 sizeZ;
+    }
+
+    Building[] public buildings;
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        uint256 _cost
+    ) ERC721(_name, _symbol) {
+        cost = _cost;
+        buildings.push(
+            Building("City Hall", address(0x0), 0, 0, 0, 10, 10, 10)
+        );
+        buildings.push(Building("Stadium", address(0x0), 0, 10, 0, 10, 5, 3));
+        buildings.push(
+            Building("University", address(0x0), 0, -10, 0, 10, 5, 3)
+        );
+        buildings.push(
+            Building("Shopping Plaza 1", address(0x0), 10, 0, 0, 5, 25, 5)
+        );
+        buildings.push(
+            Building("Shopping Plaza 2", address(0x0), -10, 0, 0, 5, 25, 5)
+        );
+    }
+}
