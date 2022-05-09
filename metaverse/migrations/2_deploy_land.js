62ee598dd90d4a750ea83ec718ce53578a23db1b
stefimaz <89653430+stefimaz@users.noreply.github.com>
2022-05-09 15:47:48 -0400

    file cleaned up

---
+++ metaverse/migrations/2_deploy_land.js
@@ -0,0 +1,8 @@
+const Land = artifacts.require("Land");
+
+module.exports = async function (deployer) {
+    const NAME = "Dapp U Building"
+    const SYMBOL = "DUB"
+    const COST = web3.utils.toWei('1', 'ether')
+    await deployer.deploy(Land, NAME, SYMBOL, COST);
+};
