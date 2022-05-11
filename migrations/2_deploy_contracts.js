4f30c5462de3486cb99b17dca7ca426f79cfbcd0
stefimaz <89653430+stefimaz@users.noreply.github.com>
2022-05-11 18:51:44 -0400

    removed extra folder

--- migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-const Land = artifacts.require("Land")
-
-module.exports = async function (deployer) {
-    const NAME = 'DApp U Buildings'
-    const SYMBOL = 'DUB'
-    const COST = web3.utils.toWei('1', 'ether')
-
-    await deployer.deploy(Land, NAME, SYMBOL, COST)
-}
